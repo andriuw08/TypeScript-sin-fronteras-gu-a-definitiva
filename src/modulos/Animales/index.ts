f7a296633b0747db6f2208559a240962343a1467
andriuw08 <andriuw.yepez@gmail.com>
2023-11-07 14:28:21 +0100

    Wildcard import y re export

---
+++ src/modulos/Animales/index.ts
@@ -0,0 +1,4 @@
+export { Animales } from "./Animales"
+export { Chanchitos } from "./Chanchitos"
+export { Caballos } from "./Caballos"
+// Esto es un re export para modularizar nuestros export de las clases
--- src/modulos/index.ts
+++ src/modulos/index.ts
@@ -9,9 +9,14 @@
 // }
 
 import { Point } from './Point' // utilizar {} es para export nombrado
-import Group from './Group'  // Sin las {} es por el export default
-
+// import Group from './Group'  // Sin las {} es por el export default
+import * as G from './Group' // El * es un wildImport
+import {Animales, Chanchitos, Caballos } from './Animales/index'
+// Si quitamos "moduleResolution": "node10",   en el tsconfig ya no es necesario colocar la extension de los archivos
 const point = new Point(1, 2)
-const group = new Group(1, 'Andriuw')
+// const group = new Group(1, 'Andriuw')
+const group = new G.default(1, 'Andriuw') //  Con el G.default es la clase como tal, si quisiesemos usar alguna de los metodos de grupo usariamos la G.metodo
 
 console.log(point, group)
+
+console.log()
