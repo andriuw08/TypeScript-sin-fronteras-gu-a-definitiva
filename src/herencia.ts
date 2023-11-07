b76d00c4872ca264b56f2860f99c79b9fa2c6390
andriuw08 <andriuw.yepez@gmail.com>
2023-11-07 11:16:22 +0100

    override

--- src/herencia.ts
+++ src/herencia.ts
@@ -12,6 +12,8 @@
  * created_by
  */
 
+// noImplicitOverride en el tsConfig sirve para que nos avise cuando necesitemos colocarle el override a un getter
+
 class DatosBasicos {
     constructor(
         public name: string,
@@ -22,6 +24,10 @@ class DatosBasicos {
 
     get fullYear() {
         return this.created_at.getFullYear()
+    }
+
+    get fullDesc() {
+        return this.name + '' + this.desc
     }
 }
 
@@ -35,6 +41,10 @@ class Producto extends DatosBasicos { // Las clases que extienden de otra son cl
         created_by: number,
     ) {
         super(name, desc, created_at, created_by) // Super es el contructor padre
+    }
+
+    override get fullDesc() {
+        return 'Producto ' + super.fullDesc
     }
 }
 
@@ -59,4 +69,4 @@ let producto1 = new Producto(100, 1, 'iPhone', 'este es un smartphone', new Date
 let categoria = new Categoria('Celulares', '', new Date, 1)
 
 categoria.agregarProducto(producto1)
-console.log(producto1, categoria)
+console.log(producto1.fullDesc, categoria)
