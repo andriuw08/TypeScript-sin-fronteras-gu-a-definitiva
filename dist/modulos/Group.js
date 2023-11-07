9b8a72702db4cb48cae1df5d41efeba9a97857b7
andriuw08 <andriuw.yepez@gmail.com>
2023-11-07 14:35:11 +0100

    js dist

---
+++ dist/modulos/Group.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Group {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+exports.default = Group;
+//# sourceMappingURL=Group.js.map
