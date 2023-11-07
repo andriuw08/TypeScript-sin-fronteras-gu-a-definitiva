cf22f1314ac17113aa1003adee820eead228a0bb
andriuw08 <andriuw.yepez@gmail.com>
2023-11-07 10:05:40 +0100

    Crear e instanciar clases

--- src/index.ts
+++ src/index.ts
@@ -1,193 +1,225 @@
-// Con tsc index.ts nos crea un archivo igual al de ts pero en js
-// Con el : despues de nuestra variable especifica que tipo de variable, para que no pueda ser asignado diferente en otro momento de nuestro codigo
-// Con tsc -init creamos un tsconfig.ts que es para poder configurar nuestro compilador
-// En el rootDir del tsconfig configuramos la direccion en la que estaran nuestros archivos ts
-// outDir es en donde colocaremos todo nuestro codigo js
-// noEmitOnError es para poder hacer que se detenga todo una vez salte un error
-// removeComments es para que remueva los comentarios
-// Una vez configurado todo esto con solo colocar tsc en la terminal nos va a hacer todos los cambios
-// sourceMap genera archivos para que le pueda decir al visualcode como se va a mapear el codigo
-// Con los puntitos rojos de la izquierda decimos cual sera el brakePoint de nuestro depurador
-// En run debug, creamos un archivo json con node js
-// Con el noImplicitReturns hacemos que salte un error cada que una de nuestras funciones no retorne algo 
+// // Con tsc index.ts nos crea un archivo igual al de ts pero en js
+// // Con el : despues de nuestra variable especifica que tipo de variable, para que no pueda ser asignado diferente en otro momento de nuestro codigo
+// // Con tsc -init creamos un tsconfig.ts que es para poder configurar nuestro compilador
+// // En el rootDir del tsconfig configuramos la direccion en la que estaran nuestros archivos ts
+// // outDir es en donde colocaremos todo nuestro codigo js
+// // noEmitOnError es para poder hacer que se detenga todo una vez salte un error
+// // removeComments es para que remueva los comentarios
+// // Una vez configurado todo esto con solo colocar tsc en la terminal nos va a hacer todos los cambios
+// // sourceMap genera archivos para que le pueda decir al visualcode como se va a mapear el codigo
+// // Con los puntitos rojos de la izquierda decimos cual sera el brakePoint de nuestro depurador
+// // En run debug, creamos un archivo json con node js
+// // Con el noImplicitReturns hacemos que salte un error cada que una de nuestras funciones no retorne algo 
 
-let mensaje: string = "Hola mundo"
+// let mensaje: string = "Hola mundo"
 
-console.log(mensaje)
+// console.log(mensaje)
 
-mensaje = "chao mundo"
+// mensaje = "chao mundo"
 
-/** 
- * Tipos de datos en TS
- * any: cualquier tipo de dato
- * unknow
- * never
- * arrays
- * tuplas
- * Enums
- * 
- * Tipos inferidos
-*/
+// /** 
+//  * Tipos de datos en TS
+//  * any: cualquier tipo de dato
+//  * unknow
+//  * never
+//  * arrays
+//  * tuplas
+//  * Enums
+//  * 
+//  * Tipos inferidos
+// */
 
-let extinsionDinosaurios: number = 76_000_000
-let dinosaurioFavorito: string = "T-Rex"
-let extintos: boolean = true
+// let extinsionDinosaurios: number = 76_000_000
+// let dinosaurioFavorito: string = "T-Rex"
+// let extintos: boolean = true
 
-let animales: string[] = ['chanchito', 'feliz', 'felipe']
-let nums: number[] = [1, 2, 3]
-let checks: boolean[] = []
-let num2: Array<number> = [] // -> esta es la manera correcta de declarar un arreglo en ts
+// let animales: string[] = ['chanchito', 'feliz', 'felipe']
+// let nums: number[] = [1, 2, 3]
+// let checks: boolean[] = []
+// let num2: Array<number> = [] // -> esta es la manera correcta de declarar un arreglo en ts
 
-// animales.map(x => x.) el autocompletado va a pasar solo los metodos que coincidan con el tipo de dato
+// // animales.map(x => x.) el autocompletado va a pasar solo los metodos que coincidan con el tipo de dato
 
-let tupla: [number, string] = [1, 'chanchito feliz'] // asi creamos una tupla para poder tener arreglos de objetos
+// let tupla: [number, string] = [1, 'chanchito feliz'] // asi creamos una tupla para poder tener arreglos de objetos
 
-const chica = 's'
-const mediana = 'm'
-const grande = 'l'
-const extragrande = 'xl'
+// const chica = 's'
+// const mediana = 'm'
+// const grande = 'l'
+// const extragrande = 'xl'
 
-// PascalCase
+// // PascalCase
 
-enum Talla { Chica = 's', Mediana = 'm', Grande = 'l', ExtraGrande = 'xl' }
+// enum Talla { Chica = 's', Mediana = 'm', Grande = 'l', ExtraGrande = 'xl' }
 
-const variable1 = Talla.Chica 
+// const variable1 = Talla.Chica 
 
-const enum LoadingState { Idle, Loading, Succes, Error } // -> Es una manera mas realista de utilizar el enum, para ver el estado de carga
+// const enum LoadingState { Idle, Loading, Succes, Error } // -> Es una manera mas realista de utilizar el enum, para ver el estado de carga
 
-const estado = LoadingState.Succes
+// const estado = LoadingState.Succes
 
-const objeto = { id: 1 }
-// objeto.nombre = 'Hola mundo' // -> Esto solo en js, para agregar uno en ts tenemos que declararlo si o si
+// const objeto = { id: 1 }
+// // objeto.nombre = 'Hola mundo' // -> Esto solo en js, para agregar uno en ts tenemos que declararlo si o si
 
-const objeto2: {
-    readonly id: number, // el readonly hace que sea solo de lescutra y no se pueda reescribir 
-    nombre?: string, // El ? hace que ya no sea totalmente necesario al definir un objeto 
-} = { id: 1, nombre: 'Hola mundo'}
+// const objeto2: {
+//     readonly id: number, // el readonly hace que sea solo de lescutra y no se pueda reescribir 
+//     nombre?: string, // El ? hace que ya no sea totalmente necesario al definir un objeto 
+// } = { id: 1, nombre: 'Hola mundo'}
 
-// La manra correcta de hacerlo es darle el valor desde afuera 
+// // La manra correcta de hacerlo es darle el valor desde afuera 
 
-type Persona = {
-    readonly id: number,
-    nombre: string,
-    talla: Talla,
-}
+// type Persona = {
+//     readonly id: number,
+//     nombre: string,
+//     talla: Talla,
+// }
 
-const objeto3: Persona = {
-    id: 1,
-    nombre: 'Hola mundo',
-    talla: Talla.Mediana
-}
+// const objeto3: Persona = {
+//     id: 1,
+//     nombre: 'Hola mundo',
+//     talla: Talla.Mediana
+// }
 
-// const fn: (a: number) => number = (edad: number) => { // -> el primer parentesis lleva los parametros y el number es el tipo de valor que va a retornar
-//     if (edad > 17) {
-//         return 'puedes pasa'
+// // const fn: (a: number) => number = (edad: number) => { // -> el primer parentesis lleva los parametros y el number es el tipo de valor que va a retornar
+// //     if (edad > 17) {
+// //         return 'puedes pasa'
+// //     }
+// //     return 'no puedes pasar'
+// // }
+
+// function validaEdad(edad: number): string {
+//     if(edad > 17) {
+//         return 'puedes ingresar'
 //     }
+
 //     return 'no puedes pasar'
 // }
 
-function validaEdad(edad: number): string {
-    if(edad > 17) {
-        return 'puedes ingresar'
-    }
+// // Never funciona para las funciones que lanzan excepsiones 
 
-    return 'no puedes pasar'
-}
+// let puntaje: number | string = 98 // -> con la | asignamos varios tipos de valor, es un union type
 
-// Never funciona para las funciones que lanzan excepsiones 
+// puntaje = 'hola mundo'
 
-let puntaje: number | string = 98 // -> con la | asignamos varios tipos de valor, es un union type
+// type Animal = {
+//     id: number,
+//     estado: string
+// }
 
-puntaje = 'hola mundo'
+// type Usuario = {
+//     id: number,
+//     name: string
+// }
 
-type Animal = {
-    id: number,
-    estado: string
-}
+// let animal: Usuario | Animal = { id: 1, estado: '', name: ''} // -> otro ejemplo de union type con objetos
 
-type Usuario = {
-    id: number,
+// function suma2 (n: number | string): number { // -> otro ejemplo con funciones
+//     if(typeof n === 'number') {
+//         return n + 2
+//     }
+//     return parseInt(n + 2)
+// }
+
+// type Audit = {
+//     create_at: string,
+//     modified_at: string
+// }
+
+// type Product = {
+//     name: string
+// }
+
+// const product: Audit & Product = { // -> es uninterseption type, hace que sea del mismo tipo de los otros los tipos a los que se les esta asignando
+//     create_at: '',
+//     modified_at: '',
+//     name: ''
+// }
+
+// const nDeFibo: 0 | 3 | 1| 2 | 5 = 5 // -> Literal type, podemos asignar valores especificos que queremos que sea
+
+// function toNumber (s: string | null)  { // -> nullable
+//     if(!s) {
+//         return 0
+//     }
+//     return parseInt(s)
+// }
+
+// const n = toNumber(null)
+
+// function getUser(id: number) {
+//     if(id < 0) {
+//         null
+//     }
+//     return {
+//         id: 1,
+//         name: 'Felipe',
+//         created_at: new Date ()
+//     }
+// }
+
+// const user = getUser(-1)
+
+// console.log(user?.created_at) // -> ? es el chaining operator, primero antes de acceder al objeto evalua si existe, ya luego entre al valor
+
+
+// // nullish coalecing operator sirve para tomar valores por defecto
+
+// const difficulty: number | null = null
+
+// const user2 = {
+//     username: 'chanchuito feliz',
+//     difficulty: difficulty ?? 1, // -> lo que esta a la izquierda de ?? es el valor que asiganamos en caso de existir, el de la derecha es el de defecto
+// }
+
+// const elem: any = null
+
+// const elem1 = elem as number // -> as typpe asertion hace que nosotros asignemos el dato directamente que queremos que sea, sin importar si antes fue definido con otro tipo
+
+// const input = document.getElementById('username') as HTMLInputElement // -> de esta manera le hacemos que sea de ese tipo y asi asegurar poder acceder a su value
+// // es forzar a que sea de un tipo, pero no funciona para acceder al tipo de dato en caso de que anteriormente haya sido declarado con otro
+
+// // Type narrowing sirve para poder tener dos tipos de datos en una variable
+
+// function Lala(x:string | number) {
+//     // Sirve para llevar a un tipo por un camino en particular
+//     // if(typeof x === 'number') {
+//     //     x.
+//     // }
+//             // Asi mostramos los metodos que hay segun el tipo de dato
+//     // if (typeof x === 'string') {
+//     //     x.
+//     // }
+// }
+
+// // unknow es el remplazo de any
+
+// POO
+
+class Personaje {
+    id: number
     name: string
-}
-
-let animal: Usuario | Animal = { id: 1, estado: '', name: ''} // -> otro ejemplo de union type con objetos
-
-function suma2 (n: number | string): number { // -> otro ejemplo con funciones
-    if(typeof n === 'number') {
-        return n + 2
+    nivel: number
+    hp: number
+    constructor(id: number, name: string, nivel: number, hp: number) { // -> con el constructor es para inicializar los atributos y retornar la clase
+        this.id = id
+        this.name = name
+        this.nivel = nivel
+        this.hp = hp
     }
-    return parseInt(n + 2)
-}
 
-type Audit = {
-    create_at: string,
-    modified_at: string
-}
-
-type Product = {
-    name: string
-}
-
-const product: Audit & Product = { // -> es uninterseption type, hace que sea del mismo tipo de los otros los tipos a los que se les esta asignando
-    create_at: '',
-    modified_at: '',
-    name: ''
-}
-
-const nDeFibo: 0 | 3 | 1| 2 | 5 = 5 // -> Literal type, podemos asignar valores especificos que queremos que sea
-
-function toNumber (s: string | null)  { // -> nullable
-    if(!s) {
-        return 0
+    subirNivel(): number { // -> metodos o funciones para la clase
+        this.nivel = this.nivel + 1
+        return this.nivel
     }
-    return parseInt(s)
-}
 
-const n = toNumber(null)
-
-function getUser(id: number) {
-    if(id < 0) {
-        null
-    }
-    return {
-        id: 1,
-        name: 'Felipe',
-        created_at: new Date ()
+    cambiarHp(cantidad: number): number {
+        this.hp = this.hp + cantidad
+        return this.hp
     }
 }
 
-const user = getUser(-1)
+const personaje = new Personaje(1, 'Andriuw', 1, 100) // -> instanciacion
 
-console.log(user?.created_at) // -> ? es el chaining operator, primero antes de acceder al objeto evalua si existe, ya luego entre al valor
+personaje.cambiarHp(-10)
 
+console.log(personaje)
 
-// nullish coalecing operator sirve para tomar valores por defecto
-
-const difficulty: number | null = null
-
-const user2 = {
-    username: 'chanchuito feliz',
-    difficulty: difficulty ?? 1, // -> lo que esta a la izquierda de ?? es el valor que asiganamos en caso de existir, el de la derecha es el de defecto
-}
-
-const elem: any = null
-
-const elem1 = elem as number // -> as typpe asertion hace que nosotros asignemos el dato directamente que queremos que sea, sin importar si antes fue definido con otro tipo
-
-const input = document.getElementById('username') as HTMLInputElement // -> de esta manera le hacemos que sea de ese tipo y asi asegurar poder acceder a su value
-// es forzar a que sea de un tipo, pero no funciona para acceder al tipo de dato en caso de que anteriormente haya sido declarado con otro
-
-// Type narrowing sirve para poder tener dos tipos de datos en una variable
-
-function Lala(x:string | number) {
-    // Sirve para llevar a un tipo por un camino en particular
-    // if(typeof x === 'number') {
-    //     x.
-    // }
-            // Asi mostramos los metodos que hay segun el tipo de dato
-    // if (typeof x === 'string') {
-    //     x.
-    // }
-}
-
-// unknow es el remplazo de any
